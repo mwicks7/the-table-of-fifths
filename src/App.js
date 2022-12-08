9e5fc2fcd0c350584d1ce0f2d9c290794067cc9a
Matthew Wicks <matthewwicks@Matthews-MacBook-Pro.local>
2022-12-08 12:22:31 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+const majorKeys = ["C"];
+const minorKeys = ["C"];
+
+function App() {
+  return (
+    <div className="page">
+      <h1 className="header">Linear 5ths Table</h1>
+      <nav className="nav">
+        <ul className="nav__list">  
+          <li class="nav__list-item">C</li>
+          <li class="nav__list-item">C</li>
+        </ul>
+      </nav>
+        
+      </nav>
+    </div>
+
+  );
+}
+
+export default App;
